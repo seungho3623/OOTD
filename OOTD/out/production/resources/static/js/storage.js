92c735ea9c2e26bfbaecf397dfb7b4efa51088fd
seungho3623 <seungho3623@naver.com>
2023-05-15 00:35:23 +0900

    상세 코디 좋아요 수정
    - 상세 코디 페이지 좋아요 버튼을 통한 저장소에 코디 저장 로직 변경

--- OOTD/out/production/resources/static/js/storage.js
+++ OOTD/out/production/resources/static/js/storage.js
@@ -1,4 +1,5 @@
 const likeButtons = document.querySelectorAll('.likeButton');
+const detailLikeButton = document.querySelector('.detailLikeButton');
 let selectedItems = JSON.parse(sessionStorage.getItem('outfitStorage')) || [];
 
 //pageIndex에 따라서 buttonIndex 가 +3 이 되어야 정상적으로 저장이 가능함.
@@ -6,23 +7,46 @@ likeButtons.forEach((button, index) => {
   button.addEventListener('click', (event) => {
     const coordiDataStr = sessionStorage.getItem('coordiData');
     const coordiData = JSON.parse(coordiDataStr);
-    
+
     const adjustedIndex = (pageIndex * 3) + index;
-    const selectedItem = {
+    const likeSelectedItem = {
       thumbnail: coordiData[adjustedIndex].thumbnail,
       url: coordiData[adjustedIndex].url
     };
 
     const isDuplicate = selectedItems.find(item => {
-      return item.thumbnail === selectedItem.thumbnail && item.url === selectedItem.url;
+      return item.thumbnail === likeSelectedItem.thumbnail && item.url === likeSelectedItem.url;
     });
 
     if (!isDuplicate) {
-      selectedItems.push(selectedItem);
+      selectedItems.push(likeSelectedItem);
       sessionStorage.setItem('outfitStorage', JSON.stringify(selectedItems));
       alert('저장 완료');
     } else {
       alert('이미 선택된 아이템입니다.');
     }
   });
+});
+
+detailLikeButton.addEventListener('click', (event) => {
+  const coordiDataStr = sessionStorage.getItem('coordiData');
+  const coordiData = JSON.parse(coordiDataStr);
+
+  const adjustedIndex = (pageIndex * 3) + detailIndex;
+  const detailSelectedItem = {
+    thumbnail: coordiData[adjustedIndex].thumbnail,
+    url: coordiData[adjustedIndex].url
+  };
+
+  const isDuplicate = selectedItems.find(item => {
+    return item.thumbnail === detailSelectedItem.thumbnail && item.url === detailSelectedItem.url;
+  });
+
+  if (!isDuplicate) {
+    selectedItems.push(detailSelectedItem);
+    sessionStorage.setItem('outfitStorage', JSON.stringify(selectedItems));
+    alert('저장 완료');
+  } else {
+    alert('이미 선택된 아이템입니다.');
+  }
 });
