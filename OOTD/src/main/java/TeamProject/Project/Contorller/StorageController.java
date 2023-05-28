590294e10c73e90e57cb152fa7156e49916888f0
jdong800@naver.com <jdong800@naver.com>
2023-05-28 13:51:59 +0900

    DB 프론트엔드 연동
    DB 프론트엔드 연동

--- OOTD/src/main/java/TeamProject/Project/Contorller/StorageController.java
+++ OOTD/src/main/java/TeamProject/Project/Contorller/StorageController.java
@@ -2,12 +2,13 @@ package TeamProject.Project.Contorller;
 
 import TeamProject.Project.Entity.OutfitStorageItem;
 import TeamProject.Project.Service.OutfitStorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -28,14 +29,20 @@ public class StorageController {
 
     @GetMapping(value = "/Project/storage.do")
     public String openStoragePage(Model model) {
-        List<OutfitStorageItem> list = outfitStorageService.itemList();
-        model.addAttribute("list", list);
-
-        for(OutfitStorageItem item : list){
-            System.out.println(item);
-        }
+//        List<OutfitStorageItem> list = outfitStorageService.itemList();
+//        model.addAttribute("list", list);
+//
+//        for(OutfitStorageItem item : list){
+//            System.out.println(item);
+//        }
         //model.addAttribute("data", "TestData");
 
         return "/html/StoragePage.html";
+    }
+
+    @ResponseBody
+    @PostMapping("/Project/getStorageData")
+    private List<OutfitStorageItem> getStorageData() {
+        return outfitStorageService.itemList();
     }
 }
--- OOTD/src/main/resources/static/js/storage.js
+++ OOTD/src/main/resources/static/js/storage.js
@@ -19,9 +19,9 @@ likeButtons.forEach((button, index) => {
     });
 
     if (!isDuplicate) {
-      selectedItems.push(likeSelectedItem);
-      sessionStorage.setItem('outfitStorage', JSON.stringify(selectedItems));
-      //saveDatabase(likeSelectedItem);
+      // selectedItems.push(likeSelectedItem);
+      // sessionStorage.setItem('outfitStorage', JSON.stringify(selectedItems));
+      saveDatabase(likeSelectedItem);
       alert('저장 완료');
     } else {
       alert('이미 선택된 아이템입니다.');
@@ -44,14 +44,29 @@ detailLikeButton.addEventListener('click', (event) => {
   });
 
   if (!isDuplicate) {
-    selectedItems.push(detailSelectedItem);
-    sessionStorage.setItem('outfitStorage', JSON.stringify(selectedItems));
-    //saveDatabase(detailSelectedItem);
+    // selectedItems.push(detailSelectedItem);
+    // sessionStorage.setItem('outfitStorage', JSON.stringify(selectedItems));
+    saveDatabase(detailSelectedItem);
     alert('저장 완료');
   } else {
     alert('이미 선택된 아이템입니다.');
   }
 });
+
+function loadDatabase() {
+  $.ajax({
+    type: "POST",
+    url: "/Project/getStorageData",
+    dataType: "json",
+    success: function(data) {
+      sessionStorage.setItem("outfitStorage", JSON.stringify(data));
+      showDatabase();
+    },
+    error: function() {
+      alert("error");
+    }
+  });
+}
 
 function saveDatabase(item) {
   $.ajax({
@@ -69,7 +84,109 @@ function saveDatabase(item) {
       }
     },
     error: function() {
-      alert("An error occurred while saving data.");
+      // alert("An error occurred while saving data.");
     }
+  });
+}
+
+function showDatabase() {
+  /* DB 연동 저장소 데이터 */
+  //Mode list 값 받아오기
+  //const list = /*[[${list}]]*/ [];
+  /*
+  // outfitContainer 요소 선택
+  const outfitContainer = document.getElementById('outfitContainer');
+
+  // list 데이터를 반복하여 HTML에 추가
+  list.forEach((outfit, index) => {
+      const outfitElement = document.createElement('div');
+
+      const thumbnailImg = document.createElement('img');
+      thumbnailImg.src = outfit.thumbnail;
+      thumbnailImg.alt = 'Thumbnail';
+
+      const buttonElement = document.createElement('div');
+      const urlButton = document.createElement('button');
+      urlButton.textContent = '상세';
+      // URL 버튼 클릭 이벤트 처리
+      urlButton.addEventListener('click', () => {
+          window.open(outfit.url, '_blank');
+      });
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = '삭제';
+
+      // 삭제 버튼 클릭 이벤트 처리
+      deleteButton.addEventListener('click', () => {
+          // 해당 outfit을 outfitStorage에서 제거
+          outfitStorage.splice(index, 1);
+
+          // outfitContainer의 자식 요소 삭제
+          outfitContainer.removeChild(outfitElement);
+
+          // 변경된 outfitStorage를 sessionStorage에 저장
+          sessionStorage.setItem('outfitStorage', JSON.stringify(outfitStorage));
+
+          location.reload();
+      });
+      outfitElement.appendChild(thumbnailImg);
+      buttonElement.appendChild(urlButton);
+      buttonElement.appendChild(deleteButton);
+      outfitElement.appendChild(buttonElement);
+
+      outfitContainer.appendChild(outfitElement);
+  });
+  */
+
+  /* 기존 sessionStorage 방식 적용 */
+  // sessionStorage에서 outfitStorage 데이터 가져오기
+  const outfitStorageStr = sessionStorage.getItem('outfitStorage');
+
+  // JSON 문자열을 JavaScript 객체로 변환
+  const outfitStorage = JSON.parse(outfitStorageStr);
+
+  // outfitContainer 요소 선택
+  const outfitContainer = document.getElementById('outfitContainer');
+
+  // outfitStorage 데이터를 반복하여 HTML에 추가
+  outfitStorage.forEach((outfit, index) => {
+    const outfitElement = document.createElement('div');
+    const thumbnailImg = document.createElement('img');
+
+    thumbnailImg.src = outfit.thumbnail;
+    thumbnailImg.alt = 'Thumbnail';
+
+    const buttonElement = document.createElement('div');
+    const urlButton = document.createElement('button');
+
+    urlButton.textContent = '상세';
+    // URL 버튼 클릭 이벤트 처리
+    urlButton.addEventListener('click', () => {
+      window.open(outfit.url, '_blank');
+    });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = '삭제';
+
+    // 삭제 버튼 클릭 이벤트 처리
+    deleteButton.addEventListener('click', () => {
+      // 해당 outfit을 outfitStorage에서 제거
+      outfitStorage.splice(index, 1);
+
+      // outfitContainer의 자식 요소 삭제
+      outfitContainer.removeChild(outfitElement);
+
+      // 변경된 outfitStorage를 sessionStorage에 저장
+      sessionStorage.setItem('outfitStorage', JSON.stringify(outfitStorage));
+
+      location.reload();
+    });
+
+    outfitElement.appendChild(thumbnailImg);
+    buttonElement.appendChild(urlButton);
+    buttonElement.appendChild(deleteButton);
+    outfitElement.appendChild(buttonElement);
+
+    outfitContainer.appendChild(outfitElement);
   });
 }
